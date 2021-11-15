4cf4d68aa8f3a7ffaba0e83396c2b00996d13e59
Ba Trinh <trinhnguyenleba@gmail.com>
2021-11-15 11:15:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const post = require("./routes/post");
+const PORT = 5000;
+dotenv.config();
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", post);
+app.listen(PORT, () => {
+  console.log("server is running ");
+});
---
+++ models/User.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+    username: {
+      type: String,
+      require: true,
+      min: 3,
+      max: 50,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    authorize: {
+      type: Number,
+      enum: [1, 2, 3],
+      default: 3,
+    },
+    department: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
