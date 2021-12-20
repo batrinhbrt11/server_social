9134d664a1bfb6fd80bef2feca29796bddd6f285
Ba Trinh <trinhnguyenleba@gmail.com>
2021-12-20 16:12:35 +0700

    sdsad

--- Middleware/checktoken.js
+++ Middleware/checktoken.js
@@ -2,7 +2,43 @@ const jwt = require("jsonwebtoken");
 const _CONF = require("../common/config");
 //kiểm tra login
 const checkLogin = (req, res, next) => {
-  token = req.body.token || req.query.token || req.headers["x-access-token"];
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+  jwt.verify(token, _CONF.SECRET, function (err, decoded) {
+    if (err) {
+      console.log(err);
+      return res
+        .status(401)
+        .json({ error: true, message: "Unauthorized access.", err });
+    } else {
+      next();
+    }
+  });
+};
+//kiểm tra quyền update chủ tài khoản hoặc admin
+const checkUpdate = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+  jwt.verify(token, _CONF.SECRET, function (err, decoded) {
+    if (err) {
+      //if (err) throw new Error(err)
+      return res
+        .status(401)
+        .json({ error: true, message: "Unauthorized access.", err });
+    } else {
+      if (decoded._id === req.params.id || decoded._authorize === 1) {
+        next();
+      } else {
+        return res.status(500).json({ message: "ID sai" });
+      }
+    }
+  });
+};
+//kiểm tra quyền admin
+const checkAdmin = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
   jwt.verify(token, _CONF.SECRET, function (err, decoded) {
     if (err) {
       //if (err) throw new Error(err)
@@ -10,61 +46,31 @@ const checkLogin = (req, res, next) => {
       return res
         .status(401)
         .json({ error: true, message: "Unauthorized access.", err });
-    }
-    next();
-  });
-};
-//kiểm tra quyền update chủ tài khoản hoặc admin
-const checkUpdate = (req, res, next) => {
-  token = req.body.token || req.query.token || req.headers["x-access-token"];
-  jwt.verify(token, _CONF.SECRET, function (err, decoded) {
-    if (err) {
-      //if (err) throw new Error(err)
-      return res
-        .status(401)
-        .json({ error: true, message: "Unauthorized access.", err });
-    }
-
-    if (decoded._id === req.params.id || decoded._authorize === 1) {
-      next();
     } else {
-      return res.status(500).json({ message: "ID sai" });
-    }
-  });
-};
-//kiểm tra quyền admin
-const checkAdmin = (req, res, next) => {
-  token = req.body.token || req.query.token || req.headers["x-access-token"];
-  jwt.verify(token, _CONF.SECRET, function (err, decoded) {
-    if (err) {
-      //if (err) throw new Error(err)
-      return res
-        .status(401)
-        .json({ error: true, message: "Unauthorized access.", err });
-    }
-
-    if (decoded._authorize === 1) {
-      next();
-    } else {
-      return res.status(500).json({ message: "ID sai" });
+      if (decoded._authorize === 1) {
+        next();
+      } else {
+        return res.status(500).json({ message: "ID sai" });
+      }
     }
   });
 };
 // kiểm tra quyền đăng thông báo
 const checkFaculty = (req, res, next) => {
-  token = req.body.token || req.query.token || req.headers["x-access-token"];
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
   jwt.verify(token, _CONF.SECRET, function (err, decoded) {
     if (err) {
       //if (err) throw new Error(err)
       return res
         .status(401)
         .json({ error: true, message: "Unauthorized access.", err });
-    }
-    console.log(decoded);
-    if (decoded._authorize === 2 && decoded._faculty === req.params.id) {
-      next();
     } else {
-      return res.status(500).json({ message: "ID sai" });
+      if (decoded._authorize === 2 && decoded._faculty === req.params.id) {
+        next();
+      } else {
+        return res.status(500).json({ message: "ID sai" });
+      }
     }
   });
 };
--- routes/user.js
+++ routes/user.js
@@ -35,6 +35,38 @@ router.get("/", checkLogin, async (req, res, next) => {
       });
   }
 });
+//create user
+router.post("/", checkAdmin, async (req, res) => {
+  console.log(req.body);
+  try {
+    const checkUser = await User.findOne({
+      username: req.body.username,
+    });
+    if (!checkUser) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+      //create new user
+      const newUser = new User({
+        name: req.body.name,
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword,
+        authorize: req.body.authorize,
+      });
+
+      //save user and respond
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } else {
+      res.status("403").json("Đã có user");
+    }
+    //hashed password
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err);
+  }
+});
 //update user
 router.put("/:id", checkUpdate, async (req, res) => {
   if (req.body.password && req.body.newPassword) {
@@ -62,6 +94,12 @@ router.put("/:id", checkUpdate, async (req, res) => {
       const user = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
+      // const value = req.body.value;
+      // let array = [];
+      // value.find((v) => {
+      //   array.push(v.id);
+      // });
+
       res.status(200).json("Account has been updated");
     } catch (err) {
       return res.status(500).json(err);
@@ -70,16 +108,22 @@ router.put("/:id", checkUpdate, async (req, res) => {
 });
 
 //delete user
-router.delete("/:id", checkAdmin, async (req, res) => {
-  if (req.body.userId === req.params.id || req.body.authorize !== 1) {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("Account has been deleted");
-    } catch (err) {
-      return res.status(500).json(err);
-    }
-  } else {
-    return res.status(403).json("You can delete only your account!");
+router.delete("/", checkAdmin, async (req, res) => {
+  try {
+    // await User.findByIdAndDelete(req.body.ids);
+
+    const deleteIds = req.body.ids;
+
+    await User.deleteMany({ _id: { $in: deleteIds } });
+    User.find({})
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        return res.status(500).json(err);
+      });
+  } catch (err) {
+    return res.status(500).json(err);
   }
 });
 
